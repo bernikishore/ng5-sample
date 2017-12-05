68fd04eddaa5bcee85421bbb5221843bb56a6910
Kishore.N <arunkishore1991@gmail.com>
2017-12-05 14:30:01 +0530

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import {group, animate, query, transition, style, trigger} from '@angular/animations';
+
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  goals: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private _data: DataService
+  ) {
+    this.route.params.subscribe(res => console.log(res.id));
+  }
+
+
+  sendmeBack() {
+    this.router.navigate(['']);
+  }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+  }
+
+}
